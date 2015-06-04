04c11b85fa5f2f03501a5822de7fa50ea42c0ce3
ajwebb <webbjadam@gmail.com>
2015-06-04 11:23:07 -0700

    restructured directory
    configured application to use node/express

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+// socket io test
+var server = require('http').createServer(app);
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to html
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index.html');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
